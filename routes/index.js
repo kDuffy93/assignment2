f7744bb9023c6deea210a09f5846a556eff376e6
KyleDuffy93 <kyleduffy_83@Hotmail.com>
2017-04-10 15:58:23 -0400

    auth changes

--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,36 @@ let User = require('../models/users');
 let session = require('express-session');
 let localStrategy = require('passport-local').Strategy;
 
+
+function isLoggedIn(req, res, next) {
+   if (req.user.isAuthenticated()) {
+      return next(); // user is logged, so call the next function
+   }
+console.log('redirected from external function');
+   res.redirect('/login'); // not logged in so redirect to home
+}
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  let User = req.user;
-  if(!User)
-  {
-res.render('index', { title: 'Dashboard' });
-  }
-  else{
-    res.redirect('/login');
-  }
+router.get('/', isLoggedIn,  function(req, res, next) {
   
+
+res.render('index', { title: 'Dashboard',
+user: req.user}); 
 });
 
 
 /* GET login */
 router.get('/login', function(req, res, next) {
+
   let messages = req.session.messages || [];
+  
   
   req.session.messages = [];
   res.render('login', {
     title: 'Please Login',
-    messages: messages
-      
+    messages: messages,
+    user: null
   });
 });
 
@@ -38,9 +45,8 @@ router.post('/login', passport.authenticate('local', {
   
   successRedirect: '/',
   failureRedirect: '/login',
-    failureMessage: 'Invalid Login',
-
-}));
+    failureMessage: 'Invalid Login'
+  }));
 
 
 router.get('/logout', function(req, res, next) {
